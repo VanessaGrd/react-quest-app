cf5fa6c57cb06c05c89a32a43a29d4e64761c237
VanessaGrd <suede210810@gmail.com>
2023-03-28 16:58:24 +0200

    quete 3-react

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,25 @@
 
-export default function PokemonCard (){
-    return( 
-    <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-    <figcaption>bulbasaur</figcaption>
-</figure>
-    );
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+export default function PokemonCard() {
+    let pokemon = pokemonList[1];
+    return (
+        
+        <figure>
+            {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc}/>  : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>)
 }
+
+
+
 
