661c76ca45f935a6b6cad4e8a21575f81185b6b1
VanessaGrd <suede210810@gmail.com>
2023-03-29 17:17:14 +0200

    exercice props1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,25 +1,12 @@
+const PokemonCard = (props) => {
+  const pokemon = props.pokemon;
+  return (
+    <figure>
+      {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+};
 
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
-
-export default function PokemonCard() {
-    let pokemon = pokemonList[1];
-    return (
-        
-        <figure>
-            {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc}/>  : <p>???</p>}
-            <figcaption>{pokemon.name}</figcaption>
-        </figure>)
-}
-
-
-
+export default PokemonCard;
 
